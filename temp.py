5b76abce332542ac46c6aed51f3cc80b580619ed
Vijay-IIIT-AI <127094821+Vijay-IIIT-AI@users.noreply.github.com>
2025-02-19 14:23:40 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,1 +1,16 @@
-!pip uninstall unsloth -y && pip install --upgrade --no-cache-dir --no-deps git+https://github.com/unslothai/unsloth.git
+from unsloth import FastVisionModel
+from transformers import TextStreamer
+import torch
+from PIL import Image
+
+#LLAMA Model - "unsloth/Llama-3.2-11B-Vision-Instruct"
+#Pixtral Model - ""unsloth/Pixtral-12B-Base-2409-bnb-4bit""
+# Load the model in 16-bit
+model, tokenizer = FastVisionModel.from_pretrained(
+    "unsloth/Llama-3.2-11B-Vision-Instruct",
+    load_in_4bit=False,  # Use full precision (16-bit)   
+    device_map="auto"
+)
+
+# Enable inference mode
+FastVisionModel.for_inference(model)
