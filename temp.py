b4cff25f1eefa8b76cd55019a657870833f5c9e6
Vijay-IIIT-AI <127094821+Vijay-IIIT-AI@users.noreply.github.com>
2025-08-07 15:19:12 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -65,3 +65,14 @@ trainer.train()
 
 # ✅ Save to disk in 16bit (for vLLM)
 model.save_pretrained_merged("qwen72b_merged_fp16", tokenizer, save_method="merged_16bit")
+
+
+from unsloth import FastVisionModel # FastLanguageModel for LLMs
+import torch
+
+
+model, tokenizer = FastVisionModel.from_pretrained(
+    "unsloth/Qwen2.5-VL-7B-Instruct-bnb-4bit",
+    load_in_4bit = True, # Use 4bit to reduce memory use. False for 16bit LoRA.
+    use_gradient_checkpointing = "unsloth", # True or "unsloth" for long context
+)
