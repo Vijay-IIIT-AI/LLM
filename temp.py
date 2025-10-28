265a283a709555eca2bff978450a31585b0580cd
Vijay-IIIT-AI <127094821+Vijay-IIIT-AI@users.noreply.github.com>
2025-10-28 17:03:36 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,1 +1,1 @@
-Did wealth (high fare) consistently improve survival chances within each passenger class, or was the survival advantage of higher fares only true in 1st class?
+How many passengers had more than 2 parents/children (Parch > 2)
