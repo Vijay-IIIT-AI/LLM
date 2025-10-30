943b7872b717b77d881a7e3fbf7f28801f8d5dcb
Vijay-IIIT-AI <127094821+Vijay-IIIT-AI@users.noreply.github.com>
2025-10-30 08:54:51 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,1 +1,4 @@
-Did wealth (high fare) consistently improve survival chances within each passenger class, or was the survival advantage of higher fares only true in 1st class?
+autogen-core
+autogen-agentchat
+autogen-ext
+autogen-ext[openai]
